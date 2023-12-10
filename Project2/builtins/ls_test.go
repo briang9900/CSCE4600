5ec6bcebe57b3b0d81e85e9abf9981f4e3b67c8f
briang9900 <147281907+briang9900@users.noreply.github.com>
2023-12-10 13:16:02 -0600

    Update ls_test.go

--- Project2/builtins/ls_test.go
+++ Project2/builtins/ls_test.go
@@ -1,23 +1,13 @@
 package builtins
 
 import (
-	"bytes"
-	"io/ioutil"
 	"testing"
 )
 
 func TestLs(t *testing.T) {
-	// Redirect output to a buffer
-	buf := new(bytes.Buffer)
-	err := Ls(buf)
+	// Call Ls without any arguments
+	err := Ls()
 	if err != nil {
 		t.Errorf("Unexpected error: %v", err)
 	}
-
-	// For Ls, no need to compare output, just ensure no error occurred
-
-	// If you want to check directory contents, you can read from the buffer
-	// Example:
-	// fmt.Println("Directory contents:")
-	// fmt.Println(buf.String())
 }
